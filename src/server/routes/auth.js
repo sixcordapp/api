33e22d0591bace3ca9575d060a80beb7ba26c7e5
apollo <mxrcdev@gmail.com>
2021-04-06 13:08:59 +0200

    Update auth.js

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -3,4 +3,9 @@ const passport = require("passport");
 const { Guilds } = require("../utils/api");
 const User = require("../database/schemas/User");
 
+router.get("/", passport.authenticate('auth'));
+router.get("/callback", passport.authenticate("auth"), (req, res) => {
+  res.redirect("https://sixcord.xyz");
+});
+
 module.exports = router;
