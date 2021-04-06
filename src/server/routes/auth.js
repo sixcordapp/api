2ba648a1288731a75d6a0844b02d509829f2fcb5
apollo <mxrcdev@gmail.com>
2021-04-06 12:58:19 +0200

    Create auth.js

---
+++ src/server/routes/auth.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const passport = require("passport");
+const { Guilds } = require("../utils/api");
+const User = require("../database/schemas/User");
+
+module.exports = router;
