a62d3b4f10175c19333c495a2d3bbe57873ba5b5
apollo <mxrcdev@gmail.com>
2021-04-06 13:26:18 +0200

    Update discord.js
    its not done yet

--- src/server/strategies/discord.js
+++ src/server/strategies/discord.js
@@ -1,1 +1,12 @@
 const passport = require("passport");
+const DiscordStrategy = require("passport-discord");
+const User = require("../database/schemas/User");
+const config = require("../../../config.json");
+
+passport.use(new DiscordStrategy({
+  clientID: config.clientID,
+  clientSecret: config.clientSecret,
+  callbackURL: '/api/auth/callback',
+  scope: ['identify'],
+}, async (accessToken, refreshToken, profile, done) => {
+  const { id, username, discriminator, avatar, guilds } = profile;
